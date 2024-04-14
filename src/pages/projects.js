d129463ecd5bc1a2ccff71de8a0329312de1af88
bethanyios <bethanyioscott@hotmail.co.uk>
2024-04-14 23:13:24 +0100

    Adjust spacing

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -36,7 +36,7 @@ const Projects = () => {
     <p>Since the pilot launch with 300 prison leavers across the Kent, Surrey and Sussex probation region, 
       I have conducted research for a community rollout, including a longitudinal diary study and user interviews. 
       This effort aims to develop a whitelabel app to be adopted by various organisations seeking to benefit from it.</p>
-      <p>Alongside this project, I designed and conducted research for the new Shannon Trust website.</p>
+      <p className="last">Alongside this project, I designed and conducted research for the new Shannon Trust website.</p>
       
     <h1>Social Mobility Foundation. </h1>
     <strong><p>UX Designer & Project Manager, Website & Application Form Development (2022-present)</p></strong>
@@ -51,7 +51,7 @@ const Projects = () => {
     <p>Upon launching the transformed application form, tracked through Google Analytics, SMF observed a
       record-breaking influx of applicants within 24 hours. This outcome of boosted conversion rates stands as a testament to the
       effectiveness of the user-centered design approach I implemented.</p>
-      <p>As well as the UX designer, I was tasked with serving as Project Manager, a welcome challenge for me and an 
+      <p className="last">As well as the UX designer, I was tasked with serving as Project Manager, a welcome challenge for me and an 
         opportunity to develop management skills. I used Github to manage the full website redevelopment and continued to design the user experience in collaboration with my cross-functional team.</p>
           
     <h1> The Scouts Association. </h1>
@@ -67,7 +67,7 @@ const Projects = () => {
           create a more streamlined design as its functionality expanded, thereby enhancing the user journey's intuitiveness.</p>
     <p> Throughout the process, volunteers participated in co-design workshop sessions and were
           involved in my research. Addressing accessibility needs for the learners was key given that they were usually older or had low digital skills; I ensured we met WCAG guidelines like not demanding precision and following a linear layout.</p>
-    <p> The tool won runner-up in the Digital Leaders 100 Awards for the Digital
+    <p className="last"> The tool won runner-up in the Digital Leaders 100 Awards for the Digital
           Skills or Talent Initiative of the Year (Product or Service) category.</p>
 
     <h1> Chiltern Music Therapy.</h1>
@@ -83,7 +83,7 @@ const Projects = () => {
     <p> My research indicated the significance of streamlining the enquiry process: guiding users
           through pertinent flows such as 'who we work with' and 'where we work'. This approach
           facilitated an intuitive journey through the site tailored to their specific needs.</p>
-    <p> “From responding to our brief, to in person workshops to bring the vision to life to finally a brand
+    <p className="last"> “From responding to our brief, to in person workshops to bring the vision to life to finally a brand
           new shiny website, it was a hugely positive experience. The focus that Yalla have on the user
           when building a website really did set them apart. We are so happy with the website we now
           have and the ability we have to make changes ourselves and to request changes from the dev
@@ -133,7 +133,7 @@ const Projects = () => {
     <p> We were granted the opportunity to devise our own workshops to conduct with various charities.
         This has allowed us to build our expertise and customise each project by selecting the most
         effective design sprint exercises for the respective user groups.</p>
-    <p> Made with Miro.</p>
+    <p className="last"> Made with Miro.</p>
   
     <h1>CyberSafe Tool For Schools.</h1>
     <strong><p>UX Designer & Frontend Developer (Jun - Sept 2020)</p></strong>
@@ -142,14 +142,14 @@ const Projects = () => {
       tool is to prompt schools to prioritise cybersafety education for children and to allow them to
       showcase their certification to the public. Additionally, we have been contracted to provide
       ongoing maintenance for the app.</p>
-    <p>Made with Gatsby, Firebase, Airtable, Figma & Netlify CMS.</p>
+    <p className="last">Made with Gatsby, Firebase, Airtable, Figma & Netlify CMS.</p>
    
     <h1>Cyber Break.</h1>
     <strong><p>UX Designer & Frontend Developer (Oct 2020)</p></strong>
     <p>Another project with CyberSafeKids involved creating a family quiz designed to assess
       cybersafety practices at home. The quiz was developed for the promotion of Digital Detox Day
       in Ireland and was subsequently expanded for future use.</p>
-    <p>Made with Figma, ReactJS & Airtable.</p>
+    <p className="last">Made with Figma, ReactJS & Airtable.</p>
 
     <h1>Collaborative Digital Training.</h1>
     <strong><p>UX Designer & Frontend Developer & UX Designer (May - June 2020) </p></strong>
@@ -182,7 +182,7 @@ const Projects = () => {
     <h1>SafeSpace.</h1>
     <strong><p>Scrum Lead (Feb 2020)</p></strong>
     <p>A website for the members of the Musician’s Union to report instances of sexual harassment or abuse in the workplace.</p>
-    <p> 
+    <p className="last"> 
       Made with ReactJs, Netlify Lamdas, Airtable, Figma & Cypress.
       </p>
 
